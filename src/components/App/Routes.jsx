67554e702617dfc8ae9b24635eb030494f05c804
Uzoh Okwara <uzohokwara@gmail.com>
2020-02-13 09:12:23 +0100

    Login-Registration Done

--- src/components/App/Routes.jsx
+++ src/components/App/Routes.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NotFound from '../views/NotFound';
-import Login from '../views/Login';
-import Register from '../views/Register';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotFound from "../views/NotFound";
+import Login from "../views/Login";
 
 const Routes = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route path="/" component={NotFound} />
-        </Switch>
-    </Router>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/" component={NotFound} />
+    </Switch>
+  </Router>
 );
 
 export default Routes;
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,16 +1,74 @@
-import React, { Component } from 'react';
+import React from "react";
+import logo from "../../logo.svg";
+import "./App.scss";
+import { Login, Register } from "../auth";
 
-class Login extends Component {
-    constructor(props) {
-        super(props)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    };
+  }
 
+  changeState() {
+    const { isLogginActive } = this.state;
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
     }
 
-    render() {
-        return (
-            <div>This is the Login Page</div>
-        );
-    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+
+    return (
+      <React.Fragment>
+        <div className="text-blue-500 text-center text-3xl m-3 font-bold">
+          Pension Funds Management System
+        </div>
+        <div className="App">
+          <div className="login">
+            <div className="container">
+              {isLogginActive && (
+                <Login containerRef={ref => (this.current = ref)} />
+              )}
+              {!isLogginActive && (
+                <Register containerRef={ref => (this.current = ref)} />
+              )}
+            </div>
+            <RightSide
+              current={current}
+              containerRef={ref => (this.rightSide = ref)}
+              onClick={this.changeState.bind(this)}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Login;
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        {console.log(props.current)}
+        <div className="text"></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
