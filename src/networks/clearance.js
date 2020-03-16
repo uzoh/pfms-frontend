6c2f5976f9736839dfdc3ea2dfd18ad2a2ff34c4
Emmanuel Okwara <emma4real37@gmail.com>
2020-03-16 21:55:34 +0100

    Approve and Decline Clearance

--- src/networks/clearance.js
+++ src/networks/clearance.js
@@ -3,8 +3,55 @@ import axios from "axios";
 class ClearanceClient {
   static submitClearance = async payload => {
     try {
-      console.log(payload);
       await axios.post("/clearance", payload);
+      return "Success";
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          localStorage.removeItem("jwtToken");
+          window.location.href = "/";
+        }
+        const errors = error.response.data.errors;
+        return { errors };
+      }
+    }
+  };
+
+  static fetchPendingClearance = async () => {
+    try {
+      const res = await axios.get("/clearance");
+      return res.data.payload;
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          localStorage.removeItem("jwtToken");
+          window.location.href = "/";
+        }
+        const errors = error.response.data.errors;
+        return { errors };
+      }
+    }
+  };
+
+  static approveClearance = async pensionerID => {
+    try {
+      await axios.post(`/clearance/${pensionerID}`);
+      return "Success";
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          localStorage.removeItem("jwtToken");
+          window.location.href = "/";
+        }
+        const errors = error.response.data.errors;
+        return { errors };
+      }
+    }
+  };
+
+  static declineClearance = async pensionerID => {
+    try {
+      await axios.delete(`/clearance/${pensionerID}`);
       return "Success";
     } catch (error) {
       if (error.response) {
